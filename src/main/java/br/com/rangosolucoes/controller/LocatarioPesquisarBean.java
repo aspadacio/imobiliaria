abefcb1e9158d8ef70e860a4d8fc73e79ee49a64
amauri.spadacio <amauri.spadacio@tse.jus.br>
2016-07-27 18:52:53 -0300

    Locatario Pesquisar 	-criando service 	-criando tela

--- src/main/java/br/com/rangosolucoes/controller/LocatarioPesquisarBean.java
+++ src/main/java/br/com/rangosolucoes/controller/LocatarioPesquisarBean.java
@@ -6,18 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ConversationScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import lombok.Getter;
 import lombok.Setter;
+import br.com.rangosolucoes.model.TbPessoa;
 import br.com.rangosolucoes.repository.filter.LocatarioFilter;
 import br.com.rangosolucoes.service.LocatarioService;
 
-@Named
-@SessionScoped
+@Named("locatarioPesquisarBean")
+@ConversationScoped
 public class LocatarioPesquisarBean implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -26,6 +27,7 @@ public class LocatarioPesquisarBean implements Serializable{
 	LocatarioService locatarioService;
 	
 	@Getter @Setter LocatarioFilter locatarioFilter;
+	@Getter @Setter List<TbPessoa> locatarios;
 	
 	//Listas de dados buscados no banco para serem usados como pesquisa
 	@Getter @Setter private List<String> cpfs;
@@ -35,15 +37,32 @@ public class LocatarioPesquisarBean implements Serializable{
 	
 	@PostConstruct
 	private void init(){
+		initClean();
+		
+		//Buscando informações inseridas no DB
+		retornarFiltrosDB();
+	}
+	
+	//Método responsável por limpar/inicializar os atributos locais.
+	private void initClean() {
 		locatarioFilter = new LocatarioFilter();
+		locatarios = new ArrayList<TbPessoa>();
 		
 		cpfs = new ArrayList<String>();
 		cnpjs = new ArrayList<String>();
 		municipios = new ArrayList<String>();
 		ufs = new ArrayList<String>();
-		
-		//Buscando informações inseridas no DB
-		retornarFiltrosDB();
+	}
+	
+	/**
+	 * Método responsável por cadastrar um novo Locatário
+	 * redireciona para página de cadastro
+	 * @throws IOException 
+	 * */
+	public void cadastrar() throws IOException{
+		FacesContext.getCurrentInstance().getExternalContext().redirect("LocatarioCadastrar.xhtml"); //change context
+		return; //to the method's invoking stops
+		//return "/imobiliaria/LocatarioCadastrar?faces-redirect=true";
 	}
 	
 	/**
@@ -58,20 +77,27 @@ public class LocatarioPesquisarBean implements Serializable{
 		ufs = locatarioService.retornarUfs();
 	}
 
-	/**
-	 * Método responsável por cadastrar um novo Locatário
-	 * redireciona para página de cadastro
-	 * @throws IOException 
-	 * */
-	public void cadastrar() throws IOException{
-		FacesContext.getCurrentInstance().getExternalContext().redirect("LocatarioCadastrar.xhtml"); //change context
-		return; //to the method's invoking stops
-	}
 	
 	/**
-	 * Método responsável por pesquisar um Locatário
+	 * Método responsável por pesquisar um(ns) Locatário(s) conforme filtro {@link LocatarioFilter}
 	 * */
 	public void pesquisar(){
-		
+		prepararFiltro();
+		locatarios = locatarioService.buscaPessoas(this.locatarioFilter);
+	}
+
+	/**
+	 * Método responsável por alterar {@link String} para lowerCase e tirar caracteres indevidos.
+	 * */
+	private void prepararFiltro() {
+		if(locatarioFilter.getNome() != null && locatarioFilter.getNome() != ""){
+			locatarioFilter.setNome(locatarioFilter.getNome().toLowerCase());			
+		}
+		if(locatarioFilter.getUf() != null && locatarioFilter.getUf() != ""){
+			locatarioFilter.setUf(locatarioFilter.getUf().toLowerCase());			
+		}
+		/*if(locatarioFilter.getCpf() != null && locatarioFilter.getCpf() != ""){
+			
+		}*/
 	}
 }
--- src/main/java/br/com/rangosolucoes/repository/LocatarioRepository.java
+++ src/main/java/br/com/rangosolucoes/repository/LocatarioRepository.java
@@ -6,8 +6,19 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.rangosolucoes.model.TbBairro;
+import br.com.rangosolucoes.model.TbEnderecoPessoa;
+import br.com.rangosolucoes.model.TbMunicipio;
 import br.com.rangosolucoes.model.TbPessoa;
+import br.com.rangosolucoes.model.TbPessoaFisica;
+import br.com.rangosolucoes.model.TbPessoaJuridica;
 import br.com.rangosolucoes.model.TbPessoaTelefone;
+import br.com.rangosolucoes.repository.filter.LocatarioFilter;
 
 public class LocatarioRepository implements Serializable {
 
@@ -16,21 +27,71 @@ public class LocatarioRepository implements Serializable {
 	@Inject
 	private EntityManager manager;
 
-	public void salvarPessoa(TbPessoa pessoa, Boolean isPessoaFisica) {
+	/**
+	 * Método responsável por salvar um Locatário com suas devidas entidades {@link TbPesso}, {@link TbPessoaFisica}, {@link TbPessoaJuridica}, {@link TbPessoaTelefone} e {@link TbMunicipio}
+	 * @param bairros 
+	 * @param municipios 
+	 * @param enderecos 
+	 * @param phones 
+	 * @param isSameAddress - diz se "Endereco" e "Endereco Cobranca" são iguais. 
+	 * 
+	 * */
+	public void salvarPessoa(TbPessoa pessoa, List<TbPessoaTelefone> phones,
+			List<TbEnderecoPessoa> enderecos, List<TbMunicipio> municipios,
+			List<TbBairro> bairros, Boolean isPessoaFisica,
+			Boolean isSameAddress) {
 		//Persistindo Pessoa Física / Jurídica
 		if(isPessoaFisica){
-			manager.merge(pessoa.getTbPessoaFisica());
+			manager.merge(pessoa.getTbPessoaFisica()); //TbPessoaFisica
 		}else{
-			manager.merge(pessoa.getTbPessoaJuridica());
+			manager.merge(pessoa.getTbPessoaJuridica()); //TbPessoaJuridica
 		}
 		
 		//Persistindo Pessoa
-		TbPessoa pessoaPesisted = manager.merge(pessoa);
+		TbPessoa pessoaPersisted = manager.merge(pessoa); //TbPessoa
+		
+		//Persistindo Endereço completo: Municipio, Bairro e Endereço
+		TbMunicipio municipioPersisted = null;
+		TbBairro bairroPersisted = null;
+		if(!isSameAddress){
+			//Diferentes dados em "Endereço" e "Endereço Cobrança"
+			for (int i=0; i<enderecos.size(); i++) {
+				//municipio
+				municipioPersisted = manager.merge(municipios.get(i)); //TbMunicipio
+				
+				//bairro
+				bairros.get(i).setTbMunicipio(municipioPersisted);
+				bairroPersisted = manager.merge(bairros.get(i)); //TbBairro
+				
+				//endereco
+				enderecos.get(i).setTbMunicipio(municipioPersisted);
+				enderecos.get(i).setTbBairro(bairroPersisted);
+				enderecos.get(i).setTbPessoa(pessoaPersisted);
+				manager.merge(enderecos.get(i)); //TbEnderecoPesso
+			}
+		}else{
+			//**Mesmo Endereço - então não muda o Municipio (TbMunicipio) e o Bairro (TbBairro)**//
+			//municipio
+			municipioPersisted = manager.merge(municipios.get(0)); //TbMunicipio
+			
+			//bairro
+			bairros.get(0).setTbMunicipio(municipioPersisted);
+			bairroPersisted = manager.merge(bairros.get(0)); //TbBairro
+			
+			for (int i=0; i<enderecos.size(); i++) {
+				//endereco
+				enderecos.get(i).setTbMunicipio(municipioPersisted);
+				enderecos.get(i).setTbBairro(bairroPersisted);
+				enderecos.get(i).setTbPessoa(pessoaPersisted);
+				manager.merge(enderecos.get(i)); //TbEnderecoPesso
+			}
+		}
+		
 		
 		//Persistindo Telefone(s)
-		for(TbPessoaTelefone telefone : pessoa.getTbPessoaTelefones()){
-			telefone.setTbPessoa(pessoaPesisted); //Usado para setar a FK_PESSOATELEFONE_PESSOA
-			manager.merge(telefone);
+		for(TbPessoaTelefone telefone : phones){
+			telefone.setTbPessoa(pessoaPersisted); //Usado para setar a FK_PESSOATELEFONE_PESSOA
+			manager.merge(telefone); //TbPessoaTelefone
 		}
 	}
 
@@ -68,6 +129,39 @@ public class LocatarioRepository implements Serializable {
 	@SuppressWarnings("unchecked")
 	public List<String> retonarUfs() {
 		return manager.createQuery("SELECT MN.sgUf FROM TbMunicipio MN").getResultList();
+	}
+
+	/**
+	 * Método responsável por retornar Pessoas (locatários) conforme filtro {@link LocatarioFilter}
+	 * */
+	@SuppressWarnings("unchecked")
+	public List<TbPessoa> buscaPessoas(LocatarioFilter filtro) {
+		Session session = manager.unwrap(Session.class);
+		Criteria criteria = session.createCriteria(TbPessoa.class);
+		
+		//Criando os alias
+		//criteria.createAlias("tbEnderecoPessoas", "enderecos");
+		criteria.createAlias("tbPessoaFisica", "pessoaFisica");
+		
+		//add restrictions conforme filtro
+		if(filtro.getNome() != null && filtro.getNome() != ""){
+			criteria.add(Restrictions.like("pessoaFisica.noPessoaFisica", "%" + filtro.getNome() + "%"));
+		}
+		if(filtro.getCpf() != null && filtro.getCpf() != ""){
+			criteria.add(Restrictions.eq("pessoaFisica.nuCpf", filtro.getCpf()));
+		}
+		if(filtro.getCnpj() != null && filtro.getCnpj() != ""){
+			criteria.createAlias("tbPessoaJuridica", "pessoaJuridica");
+			criteria.add(Restrictions.eq("pessoaJuridica.nuCnpj", filtro.getCnpj()));
+		}
+		/*if(filtro.getMunicipio() != null && filtro.getMunicipio() != ""){
+			criteria.add(Restrictions);
+		}
+		if(filtro.getUf() != null && filtro.getUf() != ""){
+			criteria.add(Restrictions);
+		}*/
+		
+		return criteria.addOrder(Order.asc("idPessoa")).list();
 	}
 	
 }
